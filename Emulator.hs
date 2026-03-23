968d9f994f0e1bb3469b678618162e7465988dfd
Lyssia Seiden <lewy@seiden.us>
2026-03-23 14:32:11 -0700

    fix Arith instrs issuing a token on first operand arrival

--- Emulator.hs
+++ Emulator.hs
@@ -1,13 +1,18 @@
 module Emulator
   ( ArchState (..),
     Token (..),
+    Port (..),
+    squall,
+    PresenceBits (..),
     clock,
   )
 where
 
 import Data.Bits (Bits (shiftL, shiftR, (.&.)))
 import Data.IntMap qualified as M
+import Data.Maybe (fromMaybe)
 import Data.Word
+import Debug.Trace (trace)
 
 type AWord = Word64
 
@@ -24,6 +29,9 @@ data Token = Token
 data PresenceBits = Empty | Present | Constant deriving (Show, Enum)
 
 type Memory = M.IntMap (PresenceBits, AWord)
+
+memRead :: Memory -> Int -> (PresenceBits, AWord)
+memRead m a = fromMaybe (Empty, 0) (m M.!? a)
 
 data ArchState = ArchState
   { mem :: Memory,
@@ -63,6 +71,9 @@ data ISA = ISA
 applyToken :: ISA -> Token -> Memory -> (Memory, [Token])
 applyToken isa t mem = applyInstr isa t (parseInstr isa mem (stmnt t)) mem
 
+trace' :: (Show a) => a -> a
+trace' v = trace (show v) v
+
 clock :: ISA -> ArchState -> ArchState
 clock isa (ArchState mem pending) =
   let -- each cycle we process some subset of the pending tokens
@@ -71,11 +82,11 @@ clock isa (ArchState mem pending) =
       (toProcess, unprocessed) = selector isa pending
       (mem', newTokens) =
         foldl
-          (\(m, ts) f -> (fst $ f m, ts ++ snd (f m)))
+          (\(m, ts) f -> trace' (fst $ f m, ts ++ snd (f m)))
           (mem, [])
           (map (applyToken isa) toProcess) ::
           (Memory, [Token])
-   in ArchState mem' unprocessed
+   in ArchState mem' (unprocessed ++ newTokens)
 
 squall :: ISA
 squall =
@@ -106,7 +117,7 @@ squallParseTF 0b11 = Extract
 
 squallParse :: Memory -> Int -> Instruction
 squallParse mem addr =
-  let (_, val) = mem M.! addr
+  let (_, val) = mem `memRead` addr
       er = (val `shiftR` 12) .&. 0x3FF
       ea = (val `shiftR` 10) .&. 0b11
       tm = (val `shiftR` 8) .&. 0b11
@@ -155,6 +166,9 @@ matchingFunction ConstDyadic Emulator.Left Constant =
 squallALUOp :: ALUOp -> AWord -> AWord -> AWord
 squallALUOp AddVal = (+)
 
+squallALUOutputLR :: ALUOp -> (Bool, Bool)
+squallALUOutputLR AddVal = (True, False)
+
 squallApply :: Token -> Instruction -> Memory -> (Memory, [Token])
 squallApply t instr mem =
   let addr = case ea instr of
@@ -163,11 +177,11 @@ squallApply t instr mem =
             + er
               ( squallParse mem $
                   fromIntegral $
-                    snd (mem M.! stmnt t)
+                    snd (mem `memRead` stmnt t)
               )
         CodeRelative -> ctx t + er instr
         Global -> er instr
-      (pb, memVal) = mem M.! fromIntegral addr
+      (pb, memVal) = mem `memRead` fromIntegral addr
       (pb', mop, issue) = matchingFunction (tm instr) (port t) pb
       memVal' = case mop of
         Read -> memVal
@@ -189,19 +203,25 @@ squallApply t instr mem =
           let v' = squallALUOp (ao instr) vl vr
               s' = stmnt t + offset (d1 instr)
               s'' = stmnt t + offset (d2 instr)
-           in [ Token
-                  { ctx = ctx t,
-                    stmnt = s',
-                    port = p $ d1 instr,
-                    val = v'
-                  },
-                Token
-                  { ctx = ctx t,
-                    stmnt = s'',
-                    port = p $ d2 instr,
-                    val = v'
-                  }
-              ]
+              (il, ir) = squallALUOutputLR (ao instr)
+           in ( [ Token
+                    { ctx = ctx t,
+                      stmnt = s',
+                      port = p $ d1 instr,
+                      val = v'
+                    }
+                  | il && issue
+                ]
+              )
+                ++ ( [ Token
+                         { ctx = ctx t,
+                           stmnt = s'',
+                           port = p $ d2 instr,
+                           val = v'
+                         }
+                       | ir && issue
+                     ]
+                   )
         Switch -> error "todo"
         Send -> error "todo"
         Extract -> error "todo"
--- Main.hs
+++ Main.hs
@@ -1,8 +1,15 @@
-import Emulator
 import Data.IntMap qualified as M
+import Emulator
 
 main = do
-    let state = ArchState { mem = M.empty, pending = [Token 42 67 1337] }
+  let state =
+        ArchState
+          { mem = M.insert 0 (Present, 0) M.empty,
+            pending =
+              [ Token {ctx = 1, stmnt = 0, port = Emulator.Left, val = 42},
+                Token {ctx = 1, stmnt = 0, port = Emulator.Right, val = 58}
+              ]
+          }
 
-    print state
-    print $ clock state
+  print state
+  print $ clock squall state
